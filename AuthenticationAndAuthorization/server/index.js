02b49bdfed529a28d44fe4db2c54be9eee8463ad
Dinesh Kumar <132756257+dinesh-basuri@users.noreply.github.com>
2024-11-12 13:00:51 +0530

    Delete AuthenticationAndAuthorization/server directory

--- AuthenticationAndAuthorization/server/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const app = express()
-require('dotenv').config()
-const PORT = process.env.PORT
-
-app.listen(PORT,()=>{
-  console.log(`server is running on port ${PORT}`)
-})
